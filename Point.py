192e6e3af7ac626977f8de38fd5e807cd512690d
Anthony D <anthonydelgado@hotmail.com>
2018-02-19 11:29:54 -0600

    Created point class

---
+++ Point.py
@@ -0,0 +1,23 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.visited = True
+
+    # Returns the x-coordinate
+    def getXCoord(self):
+        return self.x
+    # Returns the y-coordinate
+    def getYCoord(self):
+        return self.y
+    # Change whether a point has been visited or not
+    def toogleVisited(self):
+        self.visited = not(self.visited)
+
+    # Have we visited the point or naw
+    def hasBeenVisited(self):
+        return self.visited
+    
+    
+    
