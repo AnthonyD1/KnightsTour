b799cc4e0d714f9f91005bd0c02468ef7689bb05
Anthony D <anthonydelgado@hotmail.com>
2018-02-23 01:55:06 -0600

    create 2D array

--- Board.py
+++ Board.py
@@ -37,23 +37,43 @@ def create_board(board):
 ##every grid is 64 * 64
 
 if __name__ == "__main__":
-    wn = turtle.Screen()    
+    wn = turtle.Screen()
+    wn.screensize(512, 512)
+    print(wn.screensize())
     board = turtle.Turtle()
     create_board(board)
 
     knight = turtle.Turtle()
     knight.shape("circle")
 
-    p = Point(75,75)
-    p2 = Point(150, 200)
-    print(p.x, p.y)
+    #p = Point(75,75)
+    #p2 = Point(256, -256)
+    #print(p.x, p.y)
     #knight.penup()
-    knight.goto(p.x,p.y)
-    knight.stamp()
-    knight.goto(p2.x,p2.y)
+    #knight.goto(p.x,p.y)
     #knight.stamp()
-    knight.left(3600)
-    
+    #knight.goto(p2.x,p2.y)
+    #knight.stamp()
+
+    #create and initialize a matrix of points
+    Matrix = {}
+    cellSize = 512 // 8
+    midpoint = cellSize // 2
+    for i in range (8):
+        for j in range (8):
+            # calculate x and y coordinates for points
+            x = ((cellSize * (i + 1)) - midpoint) - 256
+            y = ((cellSize * (j + 1)) - midpoint) - 256
+            Matrix[i,j] = Point(x,y)
+
+    for i in range(8):
+        for j in range (8):
+            p = Matrix[i,j]
+            print(p)
+            #knight.penup()
+            knight.goto(p.x,p.y)
+            knight.stamp()
+
 
 
     wn.exitonclick()
